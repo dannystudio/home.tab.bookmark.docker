00f1084f67285cd8d15fc493ae55b1a5c2763412
Danny Huang <dannyhuang.cch@gmail.com>
2025-02-10 20:49:50 -0500

    refactor js pull thumbnail related code to thumbnail-factory module use input url first letter for fallback thumbnail add ping to verify input url use nodemon for devtest

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const appName = 'Home Tab Bookmark';
 const version = '1.0.0';
 const express = require('express');
-const axios = require('axios');
-const sharp = require('sharp');
-const fs = require('fs');
+const ping = require('ping');
 const url = require('url');
+const fs = require('fs');
+const tf = require('./thumbnail-factory');
 
 // Available ENV
 const screenshotAPI = process.env.SCREENSHOT_API || 'false';
@@ -12,12 +12,11 @@ const openInNewTab = process.env.OPEN_IN_NEW_TAB || 'false';
 const enableBasicAuth = process.env.BASIC_AUTH || 'false';
 const username = process.env.USER_NAME || 'admin';
 const password = process.env.PASSWORD || 'admin';
-
 const port = process.env.PORT || 3000;
-const faviconAPI = 'https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=';
+
 const dataFile = './data/home-tab-bookmark-data.json';
 const thumbnailDir = './data/thumbnail';
-const sleep = ms => new Promise(res => setTimeout(res, ms));
+
 const authentication = (request, response, next) => {
     if (enableBasicAuth == 'false') {
         next();
@@ -46,82 +45,16 @@ const parseUrlParts = address => {
     return url.parse(address, true);
 };
 
-const getFileNameAndPath = address => {
+const getThumbnailProperties = (address, thumbnailType, screenshotAPI) => {
     const urlParts = parseUrlParts(address);
     const prefix = urlParts.hostname;
     !fs.existsSync(thumbnailDir) && fs.mkdirSync(thumbnailDir);
+    const originUrl = address;
     const originName = `${prefix}-${Date.now()}_origin.png`;
     const originPath = `${thumbnailDir}/${originName}`;
     const destName = `${prefix}-${Date.now()}.png`;
     const destPath = `${thumbnailDir}/${destName}`;
-    return [originName, originPath, destName, destPath];
-};
-
-const useDefaultThumbnail = (destPath, destName, originName) => {
-    try {
-        fs.copyFileSync('./default.thumbnail.png', destPath);
-        return {status: 200, message: 'Error while creating thumbnail, use default thumbnail instead.', filename: destName};
-    }
-    catch (error) {
-        console.log(`error during use default image, ${error.message}`);
-        return {status: 304, message: 'Error while creating thumbnail, please try again later.', filename: originName};
-    }
-};
-
-const downloadImage = async (address, type) => {
-    const [originName, originPath, destName, destPath] = getFileNameAndPath(address);
-    const apiUrl = type == 'icon' ? faviconAPI : ((screenshotAPI && screenshotAPI != '') ? screenshotAPI : faviconAPI);
-    try {
-        return await axios({
-            url: `${apiUrl}${address}`,
-            method: 'GET',
-            responseType: 'stream'
-        })
-        .then(async response => {
-            response.data.pipe(fs.createWriteStream(originPath));
-            const thumbnailWidth = 256;
-            const thumbnailHeight = 192;
-            const originBufferWidth = type == 'icon' ? 64 : 256;
-            const originBufferHeight = type == 'icon' ? 64 : 192;
-            const compositeTop = type == 'icon' ? ((thumbnailHeight - originBufferHeight) / 2 - 10) : 0;
-            const compositeLeft = type == 'icon' ? ((thumbnailWidth - originBufferWidth) / 2) : 0;
-            const baseImage = await sharp({
-                create: {
-                    width: thumbnailWidth,
-                    height: thumbnailHeight,
-                    channels: 4,
-                    background: { r: 0, g: 0, b: 0, alpha: 0 }
-                }
-            })
-            .png()
-            .toBuffer()
-            .catch(error => console.log(`error during creating base image, ${error.message}`));
-            const originImage = await sharp(originPath)
-            .resize(originBufferWidth, originBufferHeight)
-            .png()
-            .toBuffer()
-            .catch(error => console.log(`error during resizing origin image, ${error.message}`));
-            return await sharp(baseImage)
-            .composite([{input: originImage, top: compositeTop, left: compositeLeft}])
-            .toFile(destPath)
-            .then(() => {
-                fs.existsSync(originPath) && fs.unlinkSync(originPath);
-                return {status: 200, filename: destName};
-            })
-            .catch(error => {
-                console.log(`error during composite images, ${error.message}`);
-                return useDefaultThumbnail(destPath, destName, originName);
-            });
-        })
-        .catch(error => {
-            console.log(`error during getting image, ${error.message}`);
-            return useDefaultThumbnail(destPath, destName, originName);
-        });
-    }
-    catch (error) {
-        console.log(`error during getting image, ${error.message}`);
-        return useDefaultThumbnail(destPath, destName, originName);
-    }
+    return {originUrl, thumbnailType, originName, originPath, destName, destPath, screenshotAPI};
 };
 
 const app = express();
@@ -155,14 +88,19 @@ app.get('/process', async (request, response) => {
         const thumbnailType = queryParams.type ? queryParams.type : 'icon';
         if (queryParams.url) {
             try {
-                const result = thumbnailType == 'screen' && typeof puppeteerExtra === 'object' ?
-                await takeScreenshot(queryParams.url) :
-                await downloadImage(queryParams.url, thumbnailType);
-                if (result.status == 200 && queryParams.delete) {
-                    const deleteThumbnailPath = `${thumbnailDir}/${queryParams.delete}`;
-                    fs.existsSync(deleteThumbnailPath) && fs.unlinkSync(deleteThumbnailPath);
+                const pingresult = await ping.promise.probe(queryParams.url.replace(/(https|http):\/\//i, ''));
+                if (pingresult.alive) {
+                    const fileProps = getThumbnailProperties(queryParams.url, thumbnailType, screenshotAPI);
+                    const result = await tf.createThumbnail(fileProps);
+                    if (result.status == 200 && queryParams.delete) {
+                        const deleteThumbnailPath = `${thumbnailDir}/${queryParams.delete}`;
+                        fs.existsSync(deleteThumbnailPath) && fs.unlinkSync(deleteThumbnailPath);
+                    }
+                    response.set('Content-Type', 'text/plain').status(result.status).send(JSON.stringify(result));
                 }
-                response.set('Content-Type', 'text/plain').status(result.status).send(JSON.stringify(result));
+                else {
+                    response.set('Content-Type', 'text/plain').status(404).send(JSON.stringify({status: 404, message: 'Unable reslove the url, please check the url and then try again.'}));
+                }
             }
             catch (error) {
                 // nothing for now
---
+++ thumbnail-factory.js
@@ -0,0 +1,145 @@
+const axios = require('axios');
+const sharp = require('sharp');
+const fs = require('fs');
+
+const thumbnailWidth = 256;
+const thumbnailHeight = 192;
+const iconWidth = 64;
+const iconHeight = 64;
+let thumbnailBuffer;
+const faviconAPI = 'https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=';
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+const imageComposer = async (type, width, height, fileProps, useFallback = false) => {
+    const compositeTop = type == 'icon' ? ((thumbnailHeight - width) / 2 - 10) : 0;
+    const compositeLeft = type == 'icon' ? ((thumbnailWidth - height) / 2) : 0;  
+    const baseImage = await sharp({
+        create: {
+            width: thumbnailWidth,
+            height: thumbnailHeight,
+            channels: 4,
+            background: {
+                r: 0,
+                g: 0,
+                b: 0,
+                alpha: 0
+            }
+        }
+    })
+    .png()
+    .toBuffer()
+    .catch(error => console.log(`sharp error > create base image: ${error.message}`));
+    const sourceImage = await sharp(Buffer.from(thumbnailBuffer))
+    .png()
+    .toBuffer()
+    .catch(error => console.log(`sharp error > buffer image: ${error.message}`));
+    return await sharp(baseImage)
+    .composite([{input: sourceImage, top: compositeTop, left: compositeLeft}])
+    .toFile(fileProps.destPath)
+    .then(() => {
+        fs.existsSync(fileProps.originPath) && fs.unlinkSync(fileProps.originPath);
+        return useFallback ?
+            {status: 200, message: 'Error when creating thumbnail, use generic thumbnail instead.', filename: fileProps.destName} :
+            {status: 200, filename: fileProps.destName};
+    })
+    .catch(error => {
+        console.log(`sharp error > composite image: ${error.message}`);
+        return {status: 304, message: 'Error when creating thumbnail, please try again later.', filename: fileProps.originName};
+    });    
+};
+
+const useFallbackThumbnail = async (fileProps) => {
+    const originName = fileProps.originName;
+    const label =  originName.substring(0, 4) == 'www.' ? originName.substring(4, 5) : originName.substring(0 ,1);
+    const svg = `
+        <svg width="${iconWidth}" height="${iconHeight}">
+            <rect x="0" y="0" width="100%" height="100%" fill="#0e8dbc" rx="5" ry="5" />
+            <text x="50%" y="70%" dominant-baseline="middle" text-anchor="middle" font-size="3em" font-family="Verdana" fill="#fff">${label.toUpperCase()}</text>
+        </svg>
+        `;
+    thumbnailBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
+    return await imageComposer('icon', iconWidth, iconHeight, fileProps, true);
+};
+
+const resizeToThumbnail = async (fileProps) => {
+    const originPath = fileProps.originPath;
+    return await sharp(originPath)
+    .resize(thumbnailWidth, thumbnailHeight)
+    .png()
+    .toFile(fileProps.destPath)
+    .then(() => {        
+        fs.existsSync(originPath) && fs.unlinkSync(originPath);
+        return {status: 200, filename: fileProps.destName};
+    })
+    .catch(error => {
+        console.log(`sharp error > resize image: ${error.message}`);
+        return useFallbackThumbnail(fileProps);
+    });
+};
+
+const getImageFromAPI = async (fileProps) => {
+    const originPath = fileProps.originPath;
+    const type = fileProps.thumbnailType;
+    const apiUrl = type == 'icon' ? faviconAPI : ((fileProps.screenshotAPI && fileProps.screenshotAPI != '') ? fileProps.screenshotAPI : faviconAPI);
+    return await axios({
+        url: `${apiUrl}${fileProps.originUrl}`,
+        method: 'GET',
+        responseType: 'stream'
+    })
+    .then(async response => {
+        await response.data.pipe(fs.createWriteStream(originPath));
+        const originBufferWidth = type == 'icon' ? iconWidth : thumbnailWidth;
+        const originBufferHeight = type == 'icon' ? iconHeight : thumbnailHeight;
+        while(!fs.existsSync(originPath)) {
+            await sleep(250);
+        }
+        if (type == 'icon') {
+            thumbnailBuffer = await sharp(originPath)
+            .resize(originBufferWidth, originBufferHeight)
+            .png()
+            .toBuffer()
+            .catch(error => console.log(`sharp error > buffer icon: ${error.message}`));
+            return await imageComposer(type, originBufferWidth, originBufferHeight, fileProps);    
+        }
+        else {
+            return await resizeToThumbnail(fileProps);
+        }
+    })
+    .catch(error => {
+        console.log(`axios error > get remote image, ${error.message}`);
+        return useFallbackThumbnail(fileProps);
+    });
+};
+
+// screenshot using puppeteer
+const puppeteerExtra = require('puppeteer-extra');
+const Stealth = require('puppeteer-extra-plugin-stealth');
+puppeteerExtra.use(Stealth());
+const takeScreenshot = async(fileProps) => {
+    const originPath = fileProps.originPath;
+    const browser = await puppeteerExtra.launch();
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0');
+    await page.goto(fileProps.originUrl);
+    // await page.waitForNetworkIdle();
+    await sleep(1000);
+    await page.setViewport({width: 1024, height: 768});
+    return await page.screenshot({path: originPath})
+    .then(async () => {
+        await browser.close();
+        return await resizeToThumbnail(fileProps);
+    })
+    .catch(error => {
+        browser.close();
+        console.log(`puppeteer error > screenshot, ${error.message}`);
+        return useFallbackThumbnail(fileProps);
+    });
+};
+
+const createThumbnail = async (fileProps) => {
+    return fileProps.thumbnailType == 'screen' && typeof puppeteerExtra === 'object' ?
+    await takeScreenshot(fileProps) :
+    await getImageFromAPI(fileProps);
+};
+
+module.exports = {createThumbnail};
