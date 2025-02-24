4783d7d685743fd7cec488aebc445de50bda02d0
Danny Huang <dannyhuang.cch@gmail.com>
2025-02-23 22:19:19 -0500

    remove unused code from loader

--- src/public/loader.js
+++ src/public/loader.js
@@ -1,32 +1,15 @@
-const isFireFox = navigator.userAgent.match(/firefox/i) != null;
-const defaultTestUrl = 'http://localhost:3000';
-let browserObj, baseUrl = '';
-
-const getStorageData = async useDefault => {
-    const hostData = await browserObj.storage.local.get('home-tab-bookmark-host');
-    const hostName = hostData['home-tab-bookmark-host'];
-    const apikeyData = await browserObj.storage.local.get('home-tab-bookmark-api-key');
-    const apiKey = apikeyData['home-tab-bookmark-api-key'];
-    return {
-        hostName: hostName ? hostName : (useDefault ? defaultTestUrl : ''),
-        apiKey: apiKey ? apiKey : ''
-    };
-};
-
-const loadMainScript = () => {
-    const mainJS = document.createElement('script');
-    mainJS.src = '/script.js';
-    document.getElementsByTagName('head')[0].append(mainJS);
-};
-
-const initApp = async () => {
+let baseUrl = '';
+(async () => {
     if (document.location.href.match(/^(http|https)/) == null) {
-        browserObj = isFireFox ? browser : chrome;
+        const isFireFox = navigator.userAgent.match(/firefox/i) != null;
+        const browserObj = isFireFox ? browser : chrome;
         await browserObj.storage.local.get('home-tab-bookmark-host', data => {
-            baseUrl = data['home-tab-bookmark-host'] || defaultTestUrl;
+            baseUrl = data['home-tab-bookmark-host'] || 'http://localhost:3000';
         });
     }
-    loadMainScript();
-};
-
-initApp();
+    (() => {
+        const mainJS = document.createElement('script');
+        mainJS.src = '/script.js';
+        document.getElementsByTagName('head')[0].append(mainJS);
+    })();
+})();
